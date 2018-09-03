c3631c5c50939cda3b9bcf4dca4877fd0a28f81d
Oscar Stickland <sticosc@donvale.vic.edu.au>
2018-09-03 15:51:04 +1000

    You can now tick Duework

--- routers/ajaxRouter.js
+++ routers/ajaxRouter.js
@@ -1,9 +1,35 @@
 var express = require('express')
 var router = express.Router()
 var path = require('path');
+require('dotenv').load()
+const { Pool, Client } = require('pg')
+
+//Database
+var datauseSSL;
+if (process.env.DATASUPPORTSSL == "false") {
+  datauseSSL = false;
+} else {
+  datauseSSL = true;
+}
 
 router.get('/test', function(req, res) {
     res.send('This is a test');
+});
+
+router.get('/duework/change', function(req, res) {
+    console.log("Duework ID: " + req.query.id + ". Value: " + req.query.value);
+    const client = new Client({
+        connectionString: process.env.DATAURI,
+        ssl: datauseSSL,
+      });
+    client.connect();
+    client.query("UPDATE duework SET complete=$1 WHERE id=$2", [req.query.value, req.query.id], function(err, res) {
+        if(err) {
+            console.log("ERROR: " + err);
+        } else {
+            console.log("Updated Record");
+        }
+    });
 });
 
 module.exports = router
