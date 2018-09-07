7b50c144cf8ea70567e96832b5d199d9c6654a36
Oscar Stickland <sticosc@donvale.vic.edu.au>
2018-09-08 07:59:14 +1000

    Fixed not being able to show Duework Edit

--- public/js/duework.js
+++ public/js/duework.js
@@ -26,6 +26,11 @@ function changeDueWork(id, label, duedate) {
     console.log("Change DueWork. ID: " + id + ". Label: " + $(label).value + ". DueDate: " + $(duedate).value);
 }
 
+function ShowDueWork(id) {
+    var modal = document.getElementById('myModal' + id);
+    modal.style.display = "block";
+}
+
 function closeModal(id) {
     var modal = document.getElementById('myModal' + id);
     modal.style.display = "none";
